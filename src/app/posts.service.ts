aac15af28455f386164b3df6b7888e60f81f1e43
Jackie Yeo <jackie@ambercreative.sg>
2019-01-06 17:44:05 +0800

    wordpress api working add posts

---
+++ src/app/posts.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// import { map } from 'rxjs/operators';
+// import get from 'lodash/get';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  constructor(private http: HttpClient) {}
+  private baseURL = 'https://boxingtldr.com';
+  fetchPosts() {
+    return this.http
+      .get(`${this.baseURL}/wp-json/wp/v2/posts?_embed`);
+  }
+
+  fetchPost(post_id: string) {
+    return this.http
+      .get(`${this.baseURL}/wp-json/wp/v2/posts/${post_id}?_embed`);
+  }
+
+
+  fetchPostCategories() {
+    return this.http.get(`${this.baseURL}/wp-json/wp/v2/categories`);
+  }
+
+  fetchPostsByCategory(category_id: string) {
+    return this.http
+      .get(
+        `${this.baseURL}/wp-json/wp/v2/posts?_embed&categories=${category_id}`);
+  }
+
+}
---
+++ src/app/posts/posts.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { PostsService } from './../posts.service';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.page.html',
+  styleUrls: ['./posts.page.scss'],
+})
+export class PostsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
